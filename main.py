de585ab38145dfb07518159eadf851e6d4800526
AbstractBeats <fortnitevideocreator@gmail.com>
2023-07-16 14:39:30 +0400

    good code

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# pip install -r requirements.txt
+from gtts import gTTS as gt
+from playsound import playsound as ps
+import pyttsx3 as pyt
+import time
 
-# python venv -m envvv
-# cd envvv/Scripts
-# activate
+# strings = "I am driving a car"
+# a = gt(text=strings, lang="en", slow=False)
+# a.save('Driving.mp3')
+# time.sleep(1)
+ps('Driving.mp3')
