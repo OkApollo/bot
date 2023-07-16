ff3df9d937fff39582341e510200e5253325d738
suman.banik <suman.banik@codtrees.com>
2023-07-16 16:11:41 +0530

    speech recog

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# pip install -r requirements.txt
+from gtts import gTTS  
 
-# python venv -m envvv
-# cd envvv/Scripts
-# activate
+  
+from playsound import playsound  
+ 
+text_val = 'All the best for your exam.'  
+  
+
+language = 'en'  
+  
+
+obj = gTTS(text=text_val, lang=language, slow=False)  
+  
+
+obj.save("output/exam.mp3")  
+
+playsound("output/exam.mp3")  
